1fbb23ced5f8c4823545d511a5753c50cfca931e
navimalhotra <navimalhotra124@gmail.com>
2019-12-10 22:11:44 -0500

    After adding Sample List View to show entries on the Home Screen.

--- app/src/main/java/com/example/projectandroid/ActivityForm.java
+++ app/src/main/java/com/example/projectandroid/ActivityForm.java
@@ -3,12 +3,30 @@ package com.example.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class ActivityForm extends AppCompatActivity {
+
+    RadioGroup vehiclesRadio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        vehiclesRadio = (RadioGroup) findViewById(R.id.vehiclesRadio);
+
+        vehiclesRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.motorbike)
+                {
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -5,16 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button addButton;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("First One");
+        arrayList.add("Second One");
+        arrayList.add("Third One");
+        arrayList.add("Forth One");
+        arrayList.add("Fifth One");
+        arrayList.add("Sixth One");
+        arrayList.add("Seventh One");
+        arrayList.add("Eighth One");
+        arrayList.add("Ninth One");
+        arrayList.add("Tenth One");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
 
         addButton = (Button) findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
