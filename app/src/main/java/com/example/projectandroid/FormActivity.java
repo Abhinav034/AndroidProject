d91371c37b2868c1b1c9683f2b84c52d4e01d95d
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2019-12-15 00:41:48 -0500

    getting data from form

--- app/src/main/java/com/example/projectandroid/FormActivity.java
+++ app/src/main/java/com/example/projectandroid/FormActivity.java
@@ -3,6 +3,7 @@ package com.example.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -11,22 +12,27 @@ public class FormActivity extends AppCompatActivity {
 
     RadioGroup vehiclesRadio;
     LinearLayout linearLayout;
-    LinearLayout nprojects;
+
     Spinner emtype;
+    EditText firstname , lastname ,birthyear , salary , occupationRate , employeeId , projects, model ,plateNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        firstname = findViewById(R.id.fname);
+        lastname = findViewById(R.id.lname);
+        birthyear = findViewById(R.id.byear);
+        salary = findViewById(R.id.salary);
+        occupationRate = findViewById(R.id.orate);
+        employeeId = findViewById(R.id.eid);
+        projects = findViewById(R.id.nprojects);
+         model = findViewById(R.id.vmodel);
+         plateNumber = findViewById(R.id.pnumber);
         vehiclesRadio = (RadioGroup) findViewById(R.id.vehiclesRadio);
-
         linearLayout = (LinearLayout) findViewById(R.id.SideCar);
-
-
-
         emtype = (Spinner) findViewById(R.id.emtype);
-
         linearLayout.setVisibility(linearLayout.INVISIBLE);
 
        String selectedItem = emtype.getSelectedItem().toString();
