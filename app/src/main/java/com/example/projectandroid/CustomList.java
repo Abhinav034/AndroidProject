f2490eb2b7d9a72c83faf748698140b9ccbc336d
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2019-12-15 09:26:42 -0500

    added new files

--- app/src/main/java/com/example/projectandroid/CustomList.java
+++ app/src/main/java/com/example/projectandroid/CustomList.java
@@ -3,12 +3,24 @@ package com.example.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CustomList extends AppCompatActivity {
+
+    TextView textname , textid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_list);
+
+        textname = findViewById(R.id.textView4);
+        textid = findViewById(R.id.textView5);
+
+
+
     }
+
+
+
 }
