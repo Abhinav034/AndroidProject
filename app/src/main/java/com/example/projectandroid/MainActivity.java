31abc116f66040fc0e9c99e14302f9494f47418c
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2019-12-16 01:32:13 -0500

    corrected form layout

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -41,7 +42,19 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 openForm();
             }
         });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,EmployeeDetailsActivity.class);
 
+                Employee e  = arraylist.get(position);
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+
+            }
+        });
 
 
 
